f3ae54015c4ed92fefc2c614e4746a0706da4df1
Milana <mnadgafova47@gmail.com>
2020-12-12 18:11:24 +0700

    Добавлено пару форм

---
+++ app/src/main/java/com/example/projecttaxi/SettingsForm.java
@@ -0,0 +1,14 @@
+package com.example.projecttaxi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_form);
+    }
+}
