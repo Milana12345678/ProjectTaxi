4c7ddbb3158ac41746b94a894a656a41dfe8a407
Tema Shevchenko <svev2369@gmail.com>
2020-12-14 14:23:15 +0700

    Фотки добавили

--- app/src/main/java/com/example/projecttaxi/History.java
+++ app/src/main/java/com/example/projecttaxi/History.java
@@ -2,7 +2,10 @@ package com.example.projecttaxi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class History extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class History extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+        ImageView menu = (ImageView)findViewById(R.id.picMenu);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(History.this,GPSForm.class);
+                startActivity(intent);
+            }
+        });
     }
 }
