dc5c00cb8b0a4a27db843cfce7c3ea00c8491e45
Tema Shevchenko <svev2369@gmail.com>
2020-12-17 13:51:40 +0700

    Форма регистрации почти готова

--- app/src/main/java/com/example/projecttaxi/RegistrationForm.java
+++ app/src/main/java/com/example/projecttaxi/RegistrationForm.java
@@ -2,7 +2,10 @@ package com.example.projecttaxi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegistrationForm extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class RegistrationForm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_form);
+        Button btnReg = (Button)findViewById(R.id.btnReg);
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegistrationForm.this,LoginForm.class);
+                startActivity(intent);
+            }
+        });
     }
 }
